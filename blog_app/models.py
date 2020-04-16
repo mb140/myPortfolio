a6112e0142107bf97444785bf465cbbbbe2d3a92
mb140 <m_bilal012@hotmail.com>
2020-04-17 00:28:56 +0500

    two apps with pages

--- blog_app/models.py
+++ blog_app/models.py
@@ -2,7 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Blog(models.Model):
-    title = models.CharField(max_length=100)
-    description = models.CharField(max_length=250)
-    image = models.ImageField(upload_to='blog_app/images/')
-    url = models.URLField(blank=True)
+    title = models.CharField(max_length=200)
+    description = models.TextField(max_length=250)
+    # image = models.ImageField(upload_to='blog_app/images/')
+    # url = models.URLField(blank=True)
+    date = models.DateField(default='None')
--- blog_app/views.py
+++ blog_app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .models import Blog
 
 def all_blogs(request):
-    # blogs = Project.objects.all()
-    return render(request, 'blog_app/blogshomev1.html' )
+    # bring newest 5 blogs
+    blogs = Blog.objects.order_by('-date')[:5]
+    return render(request, 'blog_app/blogshomev1.html', {'blogs':blogs}  )
 #   , {'blogs':blogs}
