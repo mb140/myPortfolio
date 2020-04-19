c332196e7725ee5081dbf750ace8bff89acb960d
mb140 <m_bilal012@hotmail.com>
2020-04-20 00:53:08 +0500

    dynamic links

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -4,10 +4,13 @@ from django.conf import settings
 
 from . import views
 
+app_name = 'blog_app'
 urlpatterns = [
     ## So re-initialize(D) urls.py in blogs to handle different blog paths ||
     ## ## blog Home ||
     path('',views.all_blogs,name='all_blogs'),
+    path('<int:blog_id>/', views.detail, name='detail'),
+    #               detail = nickname of dynamic/ Specific blog calling
 ]
 # to upload Media to a global root/media folder
 urlpatterns += static(settings.MEDIA_URL, document_root=settings)
--- blog_app/views.py
+++ blog_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from .models import Blog
 
 def all_blogs(request):
@@ -6,3 +6,7 @@ def all_blogs(request):
     blogs = Blog.objects.order_by('-date')[:5]
     return render(request, 'blog_app/blogshomev1.html', {'blogs':blogs}  )
 #   , {'blogs':blogs}
+
+def detail(request,blog_id):
+    blog = get_object_or_404(Blog,pk=blog_id)
+    return render(request,'blog_app/detail.html',{'blog':blog})
