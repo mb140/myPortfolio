ecc36877591ff6047a9e6d6300299bf78124beb2
mb140 <m_bilal012@hotmail.com>
2020-04-28 01:14:59 +0500

    new local settings

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -129,3 +129,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+try:
+    from . local_settings import *
+except ImportError:
+    print('looks like local_setting not found !')
